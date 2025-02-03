c66cb2c6eb2151087ad21d639d35fac68555e24c
12v <11772383+12v@users.noreply.github.com>
2025-02-03 22:40:23 +0000

    Initialise tokenizer and embedding models outside function

--- embeddings.py
+++ embeddings.py
@@ -1,5 +1,7 @@
 from transformers import AutoTokenizer, BertModel
 
+tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased', use_fast=True)
+model = BertModel.from_pretrained('bert-base-uncased')
 
 def get_query_embeddings(queries):
     return get_embeddings(queries)
@@ -10,9 +12,6 @@ def get_document_embeddings(documents):
 
 
 def get_embeddings(texts):
-    tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased', use_fast=True)
-    model = BertModel.from_pretrained('bert-base-uncased')
-
     tokens = tokenizer(texts, return_tensors="pt", padding=True)
 
     embeddings = model(**tokens).last_hidden_state.mean(dim=1)
