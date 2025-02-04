598d1a04217fdaa890a73615cd66e705fa33c4f6
12v <11772383+12v@users.noreply.github.com>
2025-02-04 09:58:44 +0000

    Move embedding calculation to device

--- embeddings.py
+++ embeddings.py
@@ -1,8 +1,12 @@
 from transformers import AutoTokenizer, BertModel
+import torch
 
 tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased', use_fast=True)
 model = BertModel.from_pretrained('bert-base-uncased')
 model.eval()
+
+device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+model.to(device)
 
 def get_query_embeddings(queries):
     return get_embeddings(queries)
@@ -14,6 +18,7 @@ def get_document_embeddings(documents):
 
 def get_embeddings(texts):
     tokens = tokenizer(texts, return_tensors="pt", padding=True)
+    tokens = tokens.to(device)
 
     embeddings = model(**tokens).last_hidden_state.mean(dim=1)
-    return embeddings
+    return embeddings.cpu()
